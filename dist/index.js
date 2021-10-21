4e0ba8ec43a31b4067951222cb7766e93bf9fa5a
Hristo Ganev <hristo.ganev@pioneers.io>
2021-10-21 14:39:40 +0200

    DIGI-1111: Review please

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 _http2.default.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('<div> Hello World </div> ');
+  res.end('<div>Hello World</div>');
 }).listen(process.env.PORT || 3000, function () {
   return console.log('Listening on ' + (process.env.PORT || 3000));
 });
